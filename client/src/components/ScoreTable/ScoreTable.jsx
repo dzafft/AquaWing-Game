db2409d0b42a93cc726058415e5469b0ed3d6917
Yanay Sova <121397386+yanaysova@users.noreply.github.com>
2023-06-08 13:35:11 +0300

    scores

--- client/src/components/ScoreTable/ScoreTable.jsx
+++ client/src/components/ScoreTable/ScoreTable.jsx
@@ -7,27 +7,27 @@ const ScoreTable = () => {
       <li>
         <span>1st</span>
         <span>KingJelly</span>
-        <span className="score">49</span>
+        <span className="score">95</span>
       </li>
       <li>
         <span>2nd</span>
         <span>FishEyez</span>
-        <span className="score">42</span>
+        <span className="score">88</span>
       </li>
       <li>
         <span>3rd</span>
-        <span>Finns</span>
-        <span className="score">37</span>
+        <span>Dietrich</span>
+        <span className="score">72</span>
       </li>
       <li>
         <span>4th</span>
-        <span>Spiker</span>
-        <span className="score">34</span>
+        <span>Yanay</span>
+        <span className="score">61</span>
       </li>
       <li>
         <span>5th</span>
-        <span>Nemo</span>
-        <span className="score">26</span>
+        <span>Mihai</span>
+        <span className="score">54</span>
       </li>
     </ul>
   );
